cb13e77e3c6fbda64bd4cc41c7bb8fd14eec080f
Dominic Aveyard <dominicaveyard@gmail.com>
2013-01-29 18:14:59 -0600

    style comment clarifications. view source on paddy moved over

---
+++ public_html/thirdPartyTools/mobileViewSource/mobileViewSource.php
@@ -0,0 +1,59 @@
+<?php 
+include_once('/geshi/geshi.php');
+
+function markupLinks($html,$uri)
+{
+	preg_match('/https?:\/\/\S+?(?=\/)/i', $uri, $matches);
+	$baseUri = $matches[0];
+
+	$regex = array('/(?<=&quot;)(https?:\/\/\S+)(?=&quot;)/i',	// Absolute paths: &quot;http://asdf.com/asdf.php?id=x&ad=y&quot;
+				   '/(?<=&quot;)(\/\S+)(?=&quot;)/i');			// Relative paths: &quot;/scripts/regex-1.1.min.js&quot;
+	
+	$replc = array('<a href="$1">$1</a>',
+				   "<a href=\"$baseUri$1\">$1</a>");
+
+	return preg_replace($regex, $replc, $html);
+}
+
+$uri = $_GET['uri'];
+
+if ($_POST)
+{
+	$data = $_POST['DOM'];
+	$htmlenc = urldecode($data);
+	
+	$geshi = new GeSHi($htmlenc, 'html5');
+	$geshi->enable_keyword_links(false);
+	$geshi->enable_classes();
+	
+	$htmlenc = $geshi->parse_code();
+	
+	// Substitute tabs for 4 spaces
+	$htmlenc = str_replace("\t", '    ', $htmlenc);
+	
+	// Trim trailing spaces
+	$htmlenc = preg_replace("/[ \t]+$/", '', $htmlenc);
+	
+	// Markup URIs and paths as links
+	$htmlenc = markupLinks($htmlenc, $uri);	
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width" />
+	<title>Source of <?php echo htmlspecialchars($uri); ?></title>
+	<style>
+		pre {
+			overflow: auto;
+			white-space: pre-wrap;
+			word-wrap: break-word;
+		}
+		<?php echo $geshi->get_stylesheet(); ?>
+	</style>
+</head>
+<body>
+<?php echo $htmlenc; ?>
+</body>
+</html>
